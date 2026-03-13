99fd3a4707940440e3ac4ce17d0397333a217019
pirate6969 <devsanjai0987@gmail.com>
2026-03-13 23:42:35 +0530

    remove webpack config for vercel build

--- next.config.ts
+++ next.config.ts
@@ -9,14 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
